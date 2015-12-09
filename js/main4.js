e1b33861f59b9a00ae3627401c20b30dc46ccf5c
Timothy Barmann <tbarmann92@gmail.com>
2015-12-09 11:02:35 -0500

    Added form to add person

--- js/main4.js
+++ js/main4.js
@@ -66,10 +66,23 @@ App.Views.AddPerson = Backbone.View.extend({
 		'submit' : 'submit'
 	},
 	submit: function(e) {
-		console.log("Here");
 		e.preventDefault();
-		var thisPerson = JSON.stringify($(e.currentTarget).serializeArray());
-		console.log(thisPerson);
+		var newPerson = this.formToObj(e);
+		var person = new App.Models.Person(newPerson);
+		console.log(person);
+		this.collection.add(person);
+	},
+	formToObj: function(e) {
+		var formData = $(e.currentTarget).serializeArray();
+		// formData is array of objects, each with two properties:
+		// [{ name: "a", value: "1"},{name: "b",value: "2"}]
+		// needs to be single object:
+		// {"a":1,"b":2}
+		var obj = {};
+		$.each(formData, function(){
+			obj[this.name] = this.value;
+		});
+		return obj;
 	}
 
 
@@ -78,19 +91,21 @@ App.Views.AddPerson = Backbone.View.extend({
 
 App.Views.People = Backbone.View.extend({
 	tagName: 'ul',
+	initialize: function(){
+		this.collection.on('add',this.addOne,this);
+	},
 	render: function () {
-		this.collection.each(function(person){
-			var personView = new App.Views.Person({model:person});
-			this.$el.append(personView.render().el);
-		}, this);
+		this.collection.each(this.addOne, this);
 		return this;
+	},
+	addOne: function(person) {
+		var personView = new App.Views.Person({model:person});
+		this.$el.append(personView.render().el);
+
 	}
 });
 
-var addPersonView = new App.Views.AddPerson({ collection: peopleCollection });
-
 var peopleCollection = new App.Collections.People();
-
 
 peopleCollection.fetch({
 	url: 'js/people.json',
@@ -100,6 +115,9 @@ peopleCollection.fetch({
 		$('#people-container').append(peopleView.render().el);
 	}
 });
+
+var addPersonView = new App.Views.AddPerson({ collection: peopleCollection });
+
 
 
 
